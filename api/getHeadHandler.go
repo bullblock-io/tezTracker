548f695209f4c61845d4b09f56de4c9d3a9a8e71
Stanislav Cherviakov <stchervyakov@gmail.com>
2019-07-15 16:21:12 +0300

    more meaningful 500 error on head block error

--- api/getHeadHandler.go
+++ api/getHeadHandler.go
@@ -1,10 +1,10 @@
 package api
 
 import (
-	"github.com/bullblock-io/tezTracker/gen/restapi/operations/blocks"
 	"github.com/bullblock-io/tezTracker/api/render"
-	"github.com/bullblock-io/tezTracker/services"
+	"github.com/bullblock-io/tezTracker/gen/restapi/operations/blocks"
 	"github.com/bullblock-io/tezTracker/repos"
+	"github.com/bullblock-io/tezTracker/services"
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/jinzhu/gorm"
 	"github.com/sirupsen/logrus"
@@ -14,13 +14,13 @@ type getHeadBlockHandler struct {
 	db *gorm.DB
 }
 
-// Handle serves the Get Head Block request. 
+// Handle serves the Get Head Block request.
 func (h *getHeadBlockHandler) Handle(params blocks.GetBlocksHeadParams) middleware.Responder {
 	service := services.New(repos.New(h.db))
 	block, err := service.HeadBlock()
 	if err != nil {
 		logrus.Errorf("failed to get Head block: %s", err.Error())
-		return blocks.NewGetBlocksHeadNotFound()
+		return blocks.NewGetBlocksHeadInternalServerError()
 	}
 	return blocks.NewGetBlocksHeadOK().WithPayload(render.Block(block))
 }
