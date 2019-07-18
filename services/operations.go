d203c93d5b8013827d1b5132a912440a11accc7a
Roman Bolgov <bolgovr@gmail.com>
2019-07-18 18:15:39 +0300

    Update services/operations.go

--- services/operations.go
+++ services/operations.go
@@ -4,7 +4,7 @@ import (
 	"github.com/bullblock-io/tezTracker/models"
 )
 // GetOperations gets the operations filtering by operation kinds and blocks wiht pagination.
-func (t *TezTracker) GetOperations(kinds []string, inBlocks[]string, limit, before int64) (operations []models.Operation, err error) {
+func (t *TezTracker) GetOperations(kinds []string, inBlocks []string, limit, before int64) (operations []models.Operation, err error) {
 	r := t.repoProvider.GetOperation()
 	return r.List(kinds, inBlocks, limit, before)
 }
