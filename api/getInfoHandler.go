35e0559ad355cfd0bd4520b371c03994959473b0
Stanislav Cherviakov <stchervyakov@gmail.com>
2019-07-25 19:30:31 +0300

    staking ratio (#9)

--- api/getInfoHandler.go
+++ api/getInfoHandler.go
@@ -3,17 +3,22 @@ package api
 import (
 	"github.com/bullblock-io/tezTracker/api/render"
 	info "github.com/bullblock-io/tezTracker/gen/restapi/operations/app_info"
-	"github.com/go-openapi/runtime/middleware"
-	"github.com/sirupsen/logrus"
 	"github.com/bullblock-io/tezTracker/models"
+	"github.com/bullblock-io/tezTracker/repos"
+	"github.com/bullblock-io/tezTracker/services"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/jinzhu/gorm"
+	"github.com/sirupsen/logrus"
 )
+
 // MarketDataProvider is an interface for getting actual price and price changes.
-type MarketDataProvider interface{
+type MarketDataProvider interface {
 	GetTezosMarketData() (md models.MarketInfo, err error)
 }
 
 type getInfoHandler struct {
-provider MarketDataProvider
+	provider MarketDataProvider
+	db       *gorm.DB
 }
 
 // Handle serves the Get Info request.
@@ -23,6 +28,10 @@ func (h *getInfoHandler) Handle(params info.GetInfoParams) middleware.Responder 
 		logrus.Errorf("failed to get market data: %s", err.Error())
 		return info.NewGetInfoInternalServerError()
 	}
-
-	return info.NewGetInfoOK().WithPayload(render.Info(md))
+	service := services.New(repos.New(h.db))
+	ratio, err := service.GetStakingRatio()
+	if err != nil {
+		logrus.Errorf("failed to get staking ratio: %s", err.Error())
+	}
+	return info.NewGetInfoOK().WithPayload(render.Info(md, ratio))
 }
--- api/render/info.go
+++ api/render/info.go
@@ -4,9 +4,10 @@ import (
 	genModels "github.com/bullblock-io/tezTracker/gen/models"
 	"github.com/bullblock-io/tezTracker/models"
 )
+
 // Info renders price info into OpenAPI model.
-func Info(mi models.MarketInfo) *genModels.Info {
+func Info(mi models.MarketInfo, ratio float64) *genModels.Info {
 	p := mi.GetPrice()
 	p24 := mi.GetPriceChange()
-	return &genModels.Info{Price: &p, Price24hChange: &p24}
+	return &genModels.Info{Price: &p, Price24hChange: &p24, StakingRatio: ratio}
 }
